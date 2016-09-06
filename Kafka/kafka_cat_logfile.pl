6a3380f26bb08a52c6fcc199e65e8e6e42508c2c
cloud-zhao <wlsdhzy@163.com>
2016-09-06 10:52:08 +0800

    Support message v1

--- Kafka/kafka_cat_logfile.pl
+++ Kafka/kafka_cat_logfile.pl
@@ -3,13 +3,20 @@ use strict;
 use warnings;
 
 open my $file,"<$ARGV[0]" or die;
-my ($buff,$res,$start,$log_head,$ms_head)=("","",0,12,6);
+my ($buff,$res,$start,$log_head,$ms_head)=("","",0,12,4);
 
 while(1){
 	$res=myread(\$file,\$buff,12,$start);
-	last if $res < 12;
+	last if $res == -1;
 	my ($offset,$size)=get_head($buff);
 	$start += $log_head + $ms_head;
+
+	$res=myread(\$file,\$buff,1,$start);
+	last if $res < 1;
+	my $kfk_version=get_byte($buff);
+	#print "Kafka Version: $kfk_version\n";
+	my $v_ms_head = $kfk_version == 1 ? 10 : 2;
+	$start += $v_ms_head;
 
 	$res=myread(\$file,\$buff,4,$start);
 	last if $res < 4;
@@ -18,10 +25,11 @@ while(1){
 	$start += $ms_key_len;
 	
 	$res=myread(\$file,\$buff,4,$start);
-	last if $res < 4;
+	last if $res == -1;
 	my $ms_size=get_int($buff);
 	$start += 4;
-	my $ms_len=$size - $ms_key_len - $ms_head - 4;
+	my $ms_len=$size - $ms_key_len - $ms_head - $v_ms_head - 4;
+	#print $ms_len,"\n";
 
 	$res=myread(\$file,\$buff,$ms_len,$start);
 	last if $res == -1;
@@ -49,4 +57,9 @@ sub get_int{
 sub get_head{
 	my $buff=shift || return ();
 	return unpack("q>i>",$buff);
+}
+
+sub get_byte{
+	my $buff=shift || -1;
+	return unpack("C",$buff);
 }
