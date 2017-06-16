5865edf8b3773864722fc129b4bcc6826795a425
cloud-zhao <wlsdhzy@163.com>
2017-06-16 11:07:49 +0800

    add json_decode

--- V/JSON.pm
+++ V/JSON.pm
@@ -20,79 +20,78 @@ my $type={
 	't'	=>	6,
 	':'	=>	7
 };
-sub is_type{$type->{+shift}||$type->{int}}
-sub get_byte{substr($_[0],$_[1]++,1)}
-
-sub get_value{
-	my $t=get_byte $_[0],$_[1];
+my ($get_value,$get_array,$get_hash,$builder_hash,$builder_array);
+my $is_type=sub {$type->{+shift}||$type->{int}};
+my $get_byte=sub {substr($_[0],$_[1]++,1)};
+$get_value=sub{
+	my $t=$get_byte->($_[0],$_[1]);
 	my $i=$type->{$t};
 	if($i){
 		return '' if $_[2]==1 && $i==1;
 		return '' if $_[2]==2 && ($i==2 || $i==-3 || $i==-4);
-		$t.=get_value(@_);
+		$t.=$get_value->(@_);
 	}else{
-		$t.=get_value(@_);
+		$t.=$get_value->(@_);
 		return $t;
 	}
-}
-
-sub get_string{get_value @_,1}
-sub get_number{
+};
+my $get_string=sub{$get_value->(@_,1)};
+my $get_number=sub{
 	$_[1]--;
-	my $r=get_value $_[0],$_[1],2;
+	my $r=$get_value->($_[0],$_[1],2);
 	$_[1]--;
 	die "type error" if $r!~/^\d+$/;
 	return $r;
-}
-sub get_boolean{
+};
+my $get_boolean=sub{
 	$_[1]--;
-	my $r=get_value $_[0],$_[1],2;
+	my $r=$get_value->($_[0],$_[1],2);
 	$_[1]--;
 	die "type error" if $r ne 'true' && $r ne 'false';
 
 	my $c=sub{bless \(my $c=shift),'V::JSON::Boolean'};
 
 	return $r eq 'true'?$c->(1):$c->(0);
-}
-sub get_null{
+};
+my $get_null=sub{
 	$_[1]--;
-	my $r=get_value $_[0],$_[1],2;
+	my $r=$get_value->($_[0],$_[1],2);
 	$_[1]--;
 	die "type error" if $r ne 'null';
 	return undef;
-}
-sub get_object{
+};
+my $get_object=sub{
 	my $t=shift;
 	my $v;
 
 	if($type->{$t} && $type->{$t} == 1){
-		$v=get_string @_;
+		$v=$get_string->(@_);
 	}elsif($type->{$t} && $type->{$t} == 3){
-		$v=get_array(@_);
+		$v=$get_array->(@_);
 	}elsif($type->{$t} && $type->{$t} == 4){
-		$v=get_hash(@_);
+		$v=$get_hash->(@_);
 	}elsif($type->{$t} && $type->{$t} == 5){
-		$v=get_null @_;
+		$v=$get_null->(@_);
 	}elsif($type->{$t} && $type->{$t} == 6){
-		$v=get_boolean @_;
+		$v=$get_boolean->(@_);
 	}elsif($t =~ /\d/){
-		$v=get_number @_;
+		$v=$get_number->(@_);
 	}else{
 		die "value type error";
 	}
 
 	return $v;
-}
+};
 
-sub get_array{
-	my $t=get_byte $_[0],$_[1];
+$get_array=sub{
+	my $t=$get_byte->($_[0],$_[1]);
 	my ($r,$v)=([],undef);
 
-	$v=get_object $t,@_;
+	$v=$get_object->($t,@_);
 	push @$r,$v;
-	$t=get_byte $_[0],$_[1];
+	$t=$get_byte->($_[0],$_[1]);
 	if($type->{$t} && $type->{$t} == 2){
-		$v=get_array(@_);
+		$v=$get_array->(@_);
 		push @$r,@$v;
 		return $r;
 	}elsif($type->{$t} && $type->{$t} == -3){
@@ -100,27 +99,24 @@ sub get_array{
 	}else{
 		die "object type error";
 	}
-}
+};
 
-sub get_hash{
-	my $t=get_byte $_[0],$_[1];
+$get_hash=sub{
+	my $t=$get_byte->($_[0],$_[1]);
 	my ($r,$k,$v)=({},undef,undef);
-	#mlog(3,$t,$_[1]);
 	if($type->{$t} && $type->{$t} == 1){
-		$k=get_string @_;
-	#	mlog(3,$k,$_[1]);
-		$t=get_byte @_;
-	#	mlog(3,$k,$t,$_[1]);
+		$k=$get_string->(@_);
+		$t=$get_byte->(@_);
 		if($type->{$t} && $type->{$t} == 7){
-			$t=get_byte @_;
-			$v=get_object $t,@_;
+			$t=$get_byte->(@_);
+			$v=$get_object->($t,@_);
 		}else{
 			die "kev:value type error";
 		}
 		$r->{$k}=$v;
-		$t=get_byte @_;
+		$t=$get_byte->(@_);
 		if($type->{$t} && $type->{$t} == 2){
-			$v=get_hash(@_);
+			$v=$get_hash->(@_);
 			$r->{$_}=$v->{$_} for keys %$v;
 			return $r;
 		}elsif($type->{$t} && $type->{$t} == -4){
@@ -131,32 +127,58 @@ sub get_hash{
 	}else{
 		die "key type error";
 	}
-}
-
-my $all={};
-sub mlog{
-	my $c=(caller)[2];
-	$all->{$c}++;
-	exit if $all->{$c} == shift;
-	print "[LOG] $c ",join "\t",@_,"\n";
-}
-
-sub validate_str{
-	my $str=shift;
-	my $s=substr($str,0,1);
-	my $e=substr($str,len($str),1);
-	die "json str error" if $type->{$s} && $type->{$e} && $type->{$s}==4 && $type->{$e}==-4;
-}
+};
 
 sub json_encode{
 	my $str=shift || return {};
 	my $index=0;
-	my $t=get_byte $str,$index;
-	if($type->{$t} && $type->{$t} == 4){
-		return get_hash $str,$index;
-	}else{
-		die "type error";
-	}
+	my $t=$get_byte->($str,$index);
+	return $get_object->($t,$str,$index);
 }
+
+my $builder_scalar=sub{
+	my $s=shift;
+	no warnings 'numeric';
+	return length(""&$s)==1 && $s*0==0 && $s+0==$s ? $s : qq/"$s"/;
+};
+my $builder_undef=sub{'null'};
+my $builder_boolean=sub{$_[0]?'true':'false'};
+my $builder_object=sub{
+	return unless @_;
+	my $d=shift;
+
+	if(defined $d){
+		my $r=ref $d||'SCALAR';
+		if($r eq 'SCALAR'){
+			return $builder_scalar->($d);
+		}elsif($r eq 'V::JSON::BOOLEAN'){
+			return $builder_boolean->($$d);
+		}elsif($r eq 'ARRAY'){
+			return $builder_array->($d);
+		}elsif($r eq 'HASH'){
+			return $builder_hash->($d);
+		}else{
+			die "type error";
+		}
+	}else{
+		return $builder_undef->();
+	}
+};
+$builder_hash=sub{
+	my $d=shift;
+	my $s=[];
+
+	push @$s,$_.":".$builder_object->($d->{$_}) for keys %$d;
+	return '{'.join(',',@$s).'}';
+};
+$builder_array=sub{
+	my $d=shift;
+	my $s=[];
+
+	push @$s,$builder_object->($_) for @$d;
+	return '['.join(',',@$s).']';
+};
+
+sub json_decode{$builder_object->(shift)}
 
 1;
